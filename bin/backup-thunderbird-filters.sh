246345ec804a51ed3eb2eed8c8c476da7fba1a03
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-02-26 02:16:56 +0100

    Add email to Thunderbird filter file

--- bin/backup-thunderbird-filters.sh
+++ bin/backup-thunderbird-filters.sh
@@ -4,9 +4,31 @@
 # 21.11.2013
 #
 # Backups Thunderbird message filters to the specified server and directory.
+# localhost can be used as server to backup locally.
 
 
-FILTER_FILE=msgFilterRules.dat
+FILTER_FILE="msgFilterRules.dat"
+# *default - profile directory, it can be also read from ~/.thunderbird profiles.ini file
+PROFILE_DIR=~/.thunderbird/*default
+PREFS_FILE="$PROFILE_DIR/prefs.js"
+IMAP_DIR="$PROFILE_DIR/ImapMail"
+
+
+function getEmailForPath {
+	path=$1
+
+	# In PREFS_FILE we have inter alia something like:
+	# user_pref("mail.server.serverN.directory", "path");
+	# user_pref("mail.server.serverN.name", "example@example.com");
+
+	# Create something like user_pref("mail.server.serverN.name
+	serverNameLinePrefix=`grep $path $PREFS_FILE | cut -d, -f1 | sed "s/directory/name/"`
+
+	# Extract email address:
+	email=`grep $serverNameLinePrefix $PREFS_FILE | sed -n 's/.*"\(.*@.*\)".*/\1/p'`
+
+	echo $email
+}
 
 if [ $# -ne 2 ]
 then
@@ -16,9 +38,14 @@ fi
 server=$1
 destDir=$2
 
-cd ~/.thunderbird
-for f in `findi.sh $FILTER_FILE | grep -v \~`
+cd $IMAP_DIR
+for dir in `ls | grep -v msf`
 do
-  account=`echo $f | cut -d"/" -f4`
-  scp $f $server:$destDir/${account}_$FILTER_FILE
+	path="`pwd`/$dir"
+	email=`getEmailForPath $path`
+	src="$dir/$FILTER_FILE"
+	dst="$destDir/${email}_$FILTER_FILE"
+
+	echo "Coping $email filters"
+	scp $src $server:$dst
 done
