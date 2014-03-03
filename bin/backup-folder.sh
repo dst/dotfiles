25fd50ff962c405793c6d62bbeea03fd9acead66
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-03-03 17:20:53 +0100

    backup-eclipse.sh -> backup-folder.sh

---
+++ bin/backup-folder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+# Dariusz Stefanski
+# 22.11.2013
+#
+# Backups specfied folder on specified server.
+# It can be used to create a snaphot of Eclipse, Intellij, and so on.
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: $0 dirPath server"
+  exit 1
+fi
+dirPath=$1
+server=$2
+
+if [ ! -d $dirPath ]
+then
+  echo "Directory $dir doesn't exist"
+  exit 1
+fi
+
+dirName=`basename $dirPath`
+timestamp=`date +"%Y.%m.%d-%H.%I"`
+archiveName="$dirName-${timestamp}.zip"
+archivePath="/tmp/$archiveName"
+
+cd /tmp
+zip -r $archivePath $dirPath
+scp "$archivePath" $server:~/
