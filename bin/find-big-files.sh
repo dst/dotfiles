f5d2d5d0bc5be70e0523b5cfe49e07f688097bc8
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-03-27 14:18:13 +0100

    find-big-files.sh

---
+++ bin/find-big-files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Find big files and displays in format:
+# name: size
+#
+# Author: Dariusz Stefanski
+# Date:   27.03.2014
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 sizeThresold[MB]"
+  exit
+fi
+
+sizeThreshold=$1
+
+find  -type f -size +${sizeThreshold}M -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'
