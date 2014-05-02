e21648dae70d00beb86e4e1c096632df6030b7a2
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-05-03 00:12:03 +0200

    letter-number.py read letter from command arg

--- bin/letter-number.py
+++ bin/letter-number.py
@@ -4,6 +4,13 @@
 # 01.05.2014
 # Prints number of letter in alphabet
 
-number = ord('p') - ord('a') + 1
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: %s letter" % sys.argv[0])
+
+letter=sys.argv[1]
+number = ord(letter) - ord('a') + 1
+
 print number
 
