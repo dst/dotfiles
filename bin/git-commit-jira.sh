a93e6231ad3608fd52882f1b53dec6eb971a97cc
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-05-20 11:56:42 +0200

    Fix getting current branch

--- bin/git-commit-jira.sh
+++ bin/git-commit-jira.sh
@@ -18,7 +18,8 @@ if [ $# -eq 2 ]
 then
   case=$2
 else
-  case=`git name-rev --name-only HEAD | cut -d"_" -f1`
+  branch=`git rev-parse --abbrev-ref HEAD`
+  case=`echo $branch | cut -d"_" -f1`
 fi
 
 git commit -m "$case: $message"
