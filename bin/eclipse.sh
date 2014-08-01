36c8b0791244ad1c4ef9efb09b4f038bdd83812b
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-08-01 18:41:31 +0200

    Changes around bin and starting programs

--- bin/eclipse.sh
+++ bin/eclipse.sh
@@ -6,4 +6,4 @@
 #http://stackoverflow.com/questions/19452390/menus-dont-show-up-after-upgrading-to-ubuntu-13-10
 export UBUNTU_MENUPROXY=0
 
-~/programs/eclipse4.3/eclipse "$@"
+~/programs/eclipse/eclipse "$@"
---
+++ bin/qt-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Dariusz Stefanski
+# 2012
+# Starts programs used in Qt development
+
+assistant &
+creator &
+linguist &
+designer &
