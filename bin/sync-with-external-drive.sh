f74cf7377d40f28e17c9803493f04e9b538759a9
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-08-22 15:10:14 +0200

    rsync split logs to out and err files + progress

--- bin/sync-with-external-drive.sh
+++ bin/sync-with-external-drive.sh
@@ -4,19 +4,19 @@
 # 15.03.2012
 # Syncs local disk with external drive
 
-# TODO: use stderr and stdout files as before
-
 EX_DRIVE="/media/$USER/Expansion Drive"
 EXCLUDE=".cache"
-LOG_FILE="rsync.log"
-# Use --dry-run if needed
-RSYNC_CMD="rsync -avh --exclude=$EXCLUDE --log-file=$LOG_FILE"
+OUT_LOG_FILE="rsync-stdout.log"
+ERR_LOG_FILE="rsync-stderr.log"
+RSYNC_CMD="rsync -avh --progress --exclude=$EXCLUDE"
 
 src=$HOME
 dst="$EX_DRIVE/`hostname`"
 
 echo "Syncing $src to $dst"
 
-time $RSYNC_CMD "$src/" "$dst"
+# For redirecting details see:
+# http://stackoverflow.com/questions/692000/how-do-i-write-stderr-to-a-file-while-using-tee-with-a-pipe
+time $RSYNC_CMD "$src/" "$dst" > >(tee $OUT_LOG_FILE) 2> >(tee $ERR_LOG_FILE >&2)
 
-echo "Syncing completed. Check $LOG_FILE for details."
+echo "Syncing completed. Check $OUT_LOG_FILE and $ERR_LOG_FILE for details."
