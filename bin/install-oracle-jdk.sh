3f6845a982c09fd3ec73dfe83ab46d4bcea5ac80
Dariusz Stefanski <dariusz.stefanski@4finance.com>
2014-09-03 11:39:10 +0200

    Install Oracle JDK script

---
+++ bin/install-oracle-jdk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# Dariusz Stefanski
+# 03.09.2014
+# Installs Oracle JDK in given version
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 version(one of 6, 7, 8)"
+  exit
+fi
+version=$1
+
+sudo apt-get update
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java${version}-installer
