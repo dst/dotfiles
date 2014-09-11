096c673158544915e1038fb6c8bd5c40901598fc
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-09-11 12:53:46 +0200

    Install fish

--- bin/setup-ubuntu.sh
+++ bin/setup-ubuntu.sh
@@ -7,7 +7,7 @@
 sudo apt-get update
 
 # Basic stuff
-sudo apt-get install -y vim xclip git curl zim terminator gitk meld tree gparted openssh-server tofrodos indicator-multiload mc
+sudo apt-get install -y vim xclip git curl zim terminator gitk meld tree gparted openssh-server tofrodos indicator-multiload mc fish
 
 # Java 8
 install-oracle-jdk.sh 8
