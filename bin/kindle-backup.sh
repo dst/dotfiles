cfe69baf4acb816d662d4eb69c272e75e8e85908
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-01-09 23:33:29 +0100

    Modified scripts for backuping zte and kindle

--- bin/kindle-backup.sh
+++ bin/kindle-backup.sh
@@ -4,5 +4,8 @@
 # 07.05.2012
 # Backup kindle files
 
+BACKUP_DIR=~/backup/kindle_backup
+
 echo "Syncing Kindle"
-time rsync -avh /media/$USER/Kindle/ ~/backup/kindle_backup
+mkdir -p $BACKUP_DIR
+time rsync -avh /media/$USER/Kindle/ $BACKUP_DIR
--- bin/zte-backup.sh
+++ bin/zte-backup.sh
@@ -4,5 +4,10 @@
 # 22.02.2014
 # Backup ZTE files
 
+
+BACKUP_DIR=~/backup/zte-backup/
+
 echo "Syncing ZTE"
-time rsync -avh /media/$USER/2E24-0FE4/ ~/backup/zte-backup
+mkdir -p $BACKUP_DIR
+time rsync -avh /media/$USER/2E24-0FE4 $BACKUP_DIR
+time rsync -avh /media/$USER/9016-4EF8 $BACKUP_DIR
