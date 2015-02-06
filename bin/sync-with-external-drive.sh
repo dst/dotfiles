2e93863be1b81a30e5adc947c4ba4e409c7a41d9
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-02-06 23:08:24 +0100

    Fix name and creating dir in syncing script

--- bin/sync-with-external-drive.sh
+++ bin/sync-with-external-drive.sh
@@ -4,7 +4,7 @@
 # 15.03.2012
 # Syncs local disk with external drive
 
-EX_DRIVE="/media/$USER/ExpansionDrive"
+EX_DRIVE="/media/$USER/Expansion Drive"
 EXCLUDE=".cache"
 OUT_LOG_FILE="rsync-stdout.log"
 ERR_LOG_FILE="rsync-stderr.log"
@@ -15,6 +15,7 @@ dst="$EX_DRIVE/`hostname`"
 
 echo "Syncing $src to $dst"
 
+mkdir $dst
 # For redirecting details see:
 # http://stackoverflow.com/questions/692000/how-do-i-write-stderr-to-a-file-while-using-tee-with-a-pipe
 time $RSYNC_CMD "$src/" "$dst" > >(tee $OUT_LOG_FILE) 2> >(tee $ERR_LOG_FILE >&2)
