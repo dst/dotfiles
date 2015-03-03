dafd622733646c08696f8c588f8e443df68b0235
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-03-03 01:09:16 +0100

    Correct handling of name with spaces

--- bin/sync-with-external-drive.sh
+++ bin/sync-with-external-drive.sh
@@ -15,7 +15,7 @@ dst="$EX_DRIVE/`hostname`"
 
 echo "Syncing $src to $dst"
 
-mkdir $dst
+mkdir "$dst"
 # For redirecting details see:
 # http://stackoverflow.com/questions/692000/how-do-i-write-stderr-to-a-file-while-using-tee-with-a-pipe
 time $RSYNC_CMD "$src/" "$dst" > >(tee $OUT_LOG_FILE) 2> >(tee $ERR_LOG_FILE >&2)
