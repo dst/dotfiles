bc1d60980a7ee0f20e329a9539b644cbe07c19f1
Dariusz Stefanski <d.a.stefanski@gmail.com>
2016-06-29 01:35:14 +1000

    Script for fixing git commit author

---
+++ bin/git-fix-author.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# It fix an author of last commit and sets is as me.
+#
+# Author: Dariusz Stefanski
+# Date:   29 Jun 2016
+
+git commit --amend --author="Dariusz Stefanski <d.a.stefanski@gmail.com>"
