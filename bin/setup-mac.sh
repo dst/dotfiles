73470a740b932042d4b68747ede36cee971b6500
Dariusz Stefanski <d.a.stefanski@gmail.com>
2016-06-29 01:20:02 +1000

    Sceleton for install mac script

---
+++ bin/setup-mac.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Author: Dariusz Stefanski
+# Date	: 29.06.2014
+# It installs some programs which are useful when working on Mac computer.
+
+function install {
+    brew install $@
+}
+
