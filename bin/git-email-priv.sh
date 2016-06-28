515adc01f85e6cbe868882cc7cc9c732571a7dfc
Dariusz Stefanski <d.a.stefanski@gmail.com>
2016-06-29 01:28:34 +1000

    Script for configuring priv email in git

---
+++ bin/git-email-priv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# It sets my private email in a git configuration for a current repository.
+#
+# Author: Dariusz Stefanski
+# Date:   29 Jun 2016
+
+git config user.email d.a.stefanski@gmail.com
--- bin/git-email.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Author: Dariusz Stefanski
-# Date:   26 Mar 2012
-
-pattern=$1
-
-find -iname "*$pattern*" | xargs git blame
