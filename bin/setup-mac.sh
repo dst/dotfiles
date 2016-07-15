b142db77cc5eb5f7fcbc033053f4c8f576f20eb2
Dariusz Stefanski <d.a.stefanski@gmail.com>
2016-07-16 00:49:33 +1000

    Install jq, Alfred, Spectacle

--- bin/setup-mac.sh
+++ bin/setup-mac.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # Author: Dariusz Stefanski
-# Date	: 29.06.2014
+# Date	: 29.06.2016
 # It installs some programs which are useful when working on Mac computer.
 
 function install {
     brew install $@
 }
 
+function installFromWeb {
+	echo
+	echo "----------------------------"
+	echo "Remember to install from web: $@"
+	echo "----------------------------"
+	echo
+}
+
+install jq
+
+installFromWeb Alfred Spectacle
