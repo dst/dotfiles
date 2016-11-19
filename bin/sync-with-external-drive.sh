5e56ec9dead82a5bcd4ac60465ca1aefe05ea8b3
Dariusz Stefanski <d.a.stefanski@gmail.com>
2016-11-19 15:53:12 +1100

    Improvement for syncing script
    * detecting external drive path
    * fixed excluding directories
    * adding .Trash to excluded list

--- bin/sync-with-external-drive.sh
+++ bin/sync-with-external-drive.sh
@@ -4,18 +4,29 @@
 # 15.03.2012
 # Syncs local disk with external drive
 
-EX_DRIVE="/media/$USER/ExpansionDrive"
-EXCLUDE=".cache .gradle .m2"
+EX_DRIVE_NAME="ExpansionDrive"
+EXCLUDES="--exclude .cache --exclude .gradle --exclude .m2 --exclude .Trash"
 OUT_LOG_FILE="rsync-stdout.log"
 ERR_LOG_FILE="rsync-stderr.log"
-RSYNC_CMD="rsync -avh --progress --exclude=$EXCLUDE"
+RSYNC_CMD="rsync -avh --progress $EXCLUDES"
+
+# External drive can be mounted in a diffrent directory, depending on OS:
+# * Ubuntu: /media/$USER/ExpansionDrive
+# * Mac: /Volumes/ExpansionDrive
+#
+# mount return the following output:
+# /dev/disk2s1 on /Volumes/ExpansionDrive (ufsd_NTFS, local, nodev, nosuid, noowners)
+function findExternalDriveDir() {
+    mount | grep $EX_DRIVE_NAME | cut -d" " -f3
+}
 
 src=$HOME
-dst="$EX_DRIVE/`hostname`"
+dst="`findExternalDriveDir`/`hostname`"
 
 echo "Syncing $src to $dst"
 
 mkdir "$dst"
+cd $src
 # For redirecting details see:
 # http://stackoverflow.com/questions/692000/how-do-i-write-stderr-to-a-file-while-using-tee-with-a-pipe
 time $RSYNC_CMD "$src/" "$dst" > >(tee $OUT_LOG_FILE) 2> >(tee $ERR_LOG_FILE >&2)
