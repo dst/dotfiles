f1df2f8cd7bd81fba8d8d3c6ce0a5e40537cc7a7
NAME <d.a.stefanski@gmail.com>
2016-12-14 01:08:11 +1100

    Install js packages

--- bin/install-grunt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Dariusz Stefanski
-# 29.01.2016
-# Installs Grunt (The JavaScript Task Runner)
-
-sudo npm install -g grunt-cli
-
---
+++ bin/install-js-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Dariusz Stefanski
+# 29.01.2016
+
+# Grunt - The JavaScript Task Runner
+npm install -g grunt-cli
+# http-server is a simple, zero-configuration command-line http server
+npm install -g http-server
+# Ngrok exposes your localhost to the web. https://ngrok.com
+npm install -g ngrok
