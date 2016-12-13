e3deb387aa7a83594f5d75ca95acd861f062a45b
NAME <d.a.stefanski@gmail.com>
2016-12-14 01:09:04 +1100

    Install nvm

---
+++ bin/install-nvm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Installs nvm, Node Version Manager, https://github.com/creationix/nvm
+#
+# Dariusz Stefanski
+# 14 Dec 2016
+
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+) && . "$NVM_DIR/nvm.sh"
