61629214c3c0eaad88081569d9970f6e0c8aaa6a
Dariusz Stefanski <dstefanski.dev@gmail.com>
2017-06-29 23:39:57 +1000

    Clone dotfiles repo using https protocol then set to ssh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,10 @@
 REPO_PATH=~/myprojects/dotfiles
 
 mkdir -p $REPO_PATH
-git clone git@github.com:dst/dotfiles.git $REPO_PATH
+
+# Use only temporary https protocol, because we haven't created ssh keys yet.
+git clone https://github.com/dst/dotfiles.git $REPO_PATH
 cd $REPO_PATH
+# Set ssh protocol now, we will generate ssh keys and add them to GitHub later.
+git remote set-url origin git@github.com:dst/dotfiles.git
 ./install.sh
