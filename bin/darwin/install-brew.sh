29db74833d852c6f32a8fcf09c400c318064e0a9
Dariusz Stefanski <dstefanski.dev@gmail.com>
2017-07-02 00:38:57 +1000

    Install Homebrew only if not installed

--- bin/darwin/install-brew.sh
+++ bin/darwin/install-brew.sh
@@ -5,4 +5,11 @@
 # Dariusz Stefanski
 # 5 Jun 2017
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "Installing Homebrew ..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew has been already installed."
+fi
